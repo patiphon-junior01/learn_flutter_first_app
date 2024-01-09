350c663930d413620eefda42462c7f7267ce566b
pong dev beginer <patiphonpracticwork001@gmail.com>
2024-01-09 19:06:51 +0700

    first time push code

---
+++ flutter_project_backend/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+  "golang.org/x/crypto/bcrypt"
+)
+
+// Binding from JSON
+type Register struct {
+  Username string `json:"username" binding:"required"`
+  Password string `json:"password" binding:"required"`
+  Avatar string `json:"avatar" binding:"required"`
+  Fullname string `json:"fullname" binding:"required"`
+}
+
+// model Tbl_user
+type Tbl_user struct {
+	gorm.Model
+	Username string 
+  Password string
+  Avatar string 
+  Fullname string 
+}
+
+func main() {
+
+	// connect mysql
+	dsn := "root:7890@tcp(127.0.0.1:3306)/flutter_backend_go?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	// migrate database
+	db.AutoMigrate(&Tbl_user{})
+
+  r := gin.Default()
+
+	// post
+	r.POST("/register", func(c *gin.Context) {
+
+		var json Register
+    if err := c.ShouldBindJSON(&json); err != nil {
+      c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+      return
+    }
+
+    // if json.Username != "manu" || json.Password != "123" {
+    //   c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+    //   return
+    // }
+
+    // c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+    encryptedPassword, _ := bcrypt.GenerateFromPassword([]byte(json.Password), 10);
+
+    user := Tbl_user{
+      json.Username,
+      encryptedPassword,
+      json.Avatar,
+      json.Fullname,
+    }
+    
+    c.JSON(http.StatusOK, gin.H{
+      "message": json,
+      "encrypt": encryptedPassword,
+    })
+  })
+
+	// get
+	r.GET("/register", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+      "message": "register page",
+    })
+  })
+
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+      "message": "start migrate pages",
+    })
+  })
+
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
